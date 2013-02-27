f254558d1eb5e4b32fe44e5cf5b3ba4968100165
m2m tech <tech@m2m.at>
2013-02-27 14:31:35 +0100

    end of lecture #8

---
+++ CardViewer/CardDisplayViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardDisplayViewController.h
+//  CardViewer
+//
+//  Created by Martin Mandl on 27.02.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardDisplayViewController : UIViewController
+
+@end
---
+++ CardViewer/CardDisplayViewController.m
@@ -0,0 +1,38 @@
+//
+//  CardDisplayViewController.m
+//  CardViewer
+//
+//  Created by Martin Mandl on 27.02.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import "CardDisplayViewController.h"
+
+@interface CardDisplayViewController ()
+
+@end
+
+@implementation CardDisplayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
